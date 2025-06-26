bd6640343ffd6f33e702288e3f84247fca774bff
Simona Dobešová <160227355+smonxk@users.noreply.github.com>
2025-06-26 02:41:32 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const app = express();
 const port = 3000;
 var nestPosts = [];
 
-function NestPost(title, tags, content, posted){
+function NestPost(title, tags, content, posted, rechirped){
     this.title = title;
     this.tags = tags;
     this.content = content;
     this.posted = posted;
     this.timeStamp = new Date();
+    this.rechirped = rechirped;
 }
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -25,13 +26,17 @@ app.get("/", (req,res) => {
         });
 })
 
+app.get("")
+
+
+//routes for new post
 app.get("/newPost", (req, res) => {
     res.render("newPost.ejs");
 })
 
 
 app.post("/submit", (req, res) =>{
-    var nestPost = new NestPost(req.body["title"], req.body["tags"], req.body["content"], false);
+    var nestPost = new NestPost(req.body["title"], req.body["tags"], req.body["content"], false, false);
     nestPosts.push(nestPost);
 
     const unpostedPosts = nestPosts.filter(post => !post.posted);
@@ -41,11 +46,45 @@ app.post("/submit", (req, res) =>{
     
 });
 
+//deleting a post
 app.post("/delete-post", (req, res) => {
     const postIndex = parseInt(req.body["postIndex"]);
     nestPosts.splice(postIndex, 1)
     res.redirect("/#posts");
 });
+
+//editing a post
+app.get("/posts/:id/edit", (req, res) =>{
+    const postId = req.params.id;
+    const post = nestPosts[postId]; //udelat na tohle fci
+    res.render("editPost.ejs", {
+        post,
+        postId
+    })
+})
+
+app.post("/submitEdit/:id", (req, res) =>{
+    const postId = req.params.id;
+    const post = nestPosts[postId];
+
+    post["rechirped"] = true;
+    post["title"] = req.body["title"];
+    post["content"] = req.body["content"];
+    post["timeStamp"] = new Date();
+
+    res.redirect("/#posts")
+});
+
+//viewing a post 
+app.get("/posts/:id", (req, res) => {
+    const postId = req.params.id;
+    const post = nestPosts[postId];
+    res.render("viewPost.ejs", {
+        post, 
+        postId
+    }) 
+})
+
 
 app.listen(port, () =>{
     console.log(`App is listening on port ${port}`);
